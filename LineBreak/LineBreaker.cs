06d0c086fa935b1b6c12d5f81f7cf1bd314c10ba
Daniel Stack <dstack@microsoft.com>
2017-10-09 21:51:34 -0700

    passing all tests

--- LineBreak/LineBreaker.cs
+++ LineBreak/LineBreaker.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Text;
 
 namespace LineBreak
 {
@@ -21,6 +22,40 @@ namespace LineBreak
             {
                 return;
             }
+
+            int breakCounter = 0, i = 0;
+            StringBuilder builder = new StringBuilder();
+            char[] chars = Text.ToCharArray();
+            while(i < Text.Length)
+            {
+                if(breakCounter == columns)
+                {
+                    while(i < Text.Length && (chars[i] != ' ' &&
+                                            chars[i] != '\n'))
+                    {
+                        // move to the next "whitespace" to break on.
+                        builder.Append(chars[i]);
+                        i++;
+                    }
+                    // add the break only if we know 
+                    // we are not at the end of the text
+                    if(i < Text.Length)
+                    {
+                        builder.Append('\n');
+                    }
+                    
+                    breakCounter = 0;
+                }
+                else
+                {
+                    breakCounter++;
+                    builder.Append(chars[i]);
+                }
+
+                i++;
+            }
+
+            Text = builder.ToString();
         }
     }
 }
